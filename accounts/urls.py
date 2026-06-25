7ddbcdb568fb28c9cfa9608467ab6aa4b49a483a
NesmaAh-16 <nesmalubbad@example.com>
2026-06-26 00:41:42 +0300

    Implement database schema using Django ORM and SQLite

---
+++ accounts/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+# Define an app_name for namespacing
+app_name = 'accounts'
+
+urlpatterns = [
+    # Routes will be added here later
+    # path('login/', views.LoginView, name='login'),
+]
---
+++ operations/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+app_name = 'operations'
+
+urlpatterns = [
+    # Routes will be added here later
+    # path('orders/', views.OrderListView, name='order_list'),
+]
